eaa149eb9861b5f7fddf6f0cc4a6687db866151f
Leo Vo <leo.vo@nexcel.vn>
2017-08-04 12:31:58 +0700

    HttpRequestDemo

---
+++ HttpRequestDemo/MainForm.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace HttpRequestDemo
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.urlTextBox = new System.Windows.Forms.TextBox();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.responseTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "URL";
+            // 
+            // urlTextBox
+            // 
+            this.urlTextBox.Location = new System.Drawing.Point(47, 19);
+            this.urlTextBox.Name = "urlTextBox";
+            this.urlTextBox.Size = new System.Drawing.Size(408, 20);
+            this.urlTextBox.TabIndex = 1;
+            // 
+            // sendButton
+            // 
+            this.sendButton.Location = new System.Drawing.Point(380, 60);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(75, 23);
+            this.sendButton.TabIndex = 2;
+            this.sendButton.Text = "Send";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // responseTextBox
+            // 
+            this.responseTextBox.Location = new System.Drawing.Point(15, 104);
+            this.responseTextBox.Multiline = true;
+            this.responseTextBox.Name = "responseTextBox";
+            this.responseTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.responseTextBox.Size = new System.Drawing.Size(440, 187);
+            this.responseTextBox.TabIndex = 3;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 305);
+            this.Controls.Add(this.responseTextBox);
+            this.Controls.Add(this.sendButton);
+            this.Controls.Add(this.urlTextBox);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "mainForm";
+            this.Text = "Http Request Demo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox urlTextBox;
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.TextBox responseTextBox;
+    }
+}
+
---
+++ HttpRequestDemo/MainForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HttpRequestDemo
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private async void sendButton_Click(object sender, EventArgs e)
+        {
+            sendButton.Enabled = false;
+            try
+            {
+                var content = await DownloadPage(urlTextBox.Text);
+                responseTextBox.Text = content;
+            }
+            catch(Exception exception)
+            {
+                responseTextBox.Text = exception.Message;
+            }
+
+            sendButton.Enabled = true;
+        }
+
+        private async Task<string> DownloadPage(string url)
+        {
+            using (var client = new HttpClient())
+            {
+                using (var response = await client.GetAsync(url))
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    return content;
+                }
+            }
+        }
+    }
+}
